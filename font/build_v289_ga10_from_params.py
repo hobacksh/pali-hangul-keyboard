0f862a2c0d0f91a7d9f5e0313cc57be1ba1f23f9
Hailie <hr.yun@ad-i.net>
2026-02-15 10:54:16 +0900

    editor: expand tabs/items from 라계열 to 하계열 and add mark support (double/caron/top-tilde)

--- font/build_v289_ga10_from_params.py
+++ font/build_v289_ga10_from_params.py
@@ -14,6 +14,7 @@ BOT_TILDE = 0x0330    # 아랫물결
 BOT_TRI = 0x02D1      # 아랫 역삼각형
 DOUBLE = 0x02BA       # 자음중복
 CARON = 0x030C        # 캐론
+TOP_TILDE = 0x0303    # 윗물결
 
 with open(PARAMS_PATH, 'r', encoding='utf-8') as f:
     P = json.load(f)
@@ -43,6 +44,18 @@ if BOT_TRI not in font:
         m = psMat.compose(psMat.translate(-cx,-cy), psMat.scale(0.43))
         m = psMat.compose(m, psMat.translate(180, 390))
         g.addReference(srcg.glyphname, m); g.width = 360
+
+
+# ensure marks from Nanum when missing
+for code in (DOUBLE, CARON, TOP_TILDE):
+    if code not in font:
+        try:
+            nf = fontforge.open(os.path.expanduser('~/Library/Fonts/NanumMyeongjo-Regular.ttf'))
+            if code in nf:
+                nf.selection.select(code); nf.copy(); font.selection.select(code); font.paste()
+            nf.close()
+        except Exception:
+            pass
 
 lookup='rlig_v289_editor'
 font.addLookup(lookup,'gsub_ligature',(),(("rlig",(("DFLT",("dflt",)),("hang",("dflt",)))),))
@@ -123,7 +136,7 @@ lig_single('낀', BOT_TILDE, 'id10_tilde')
 
 # --- 나계열 ---
 lig_single('나', MACRON, 'na1_mac')
-lig_single('나', BOT_DOT, 'na2_bot')          # 윗점 -> 아랫점 변경
+lig_single('나', BOT_DOT, 'na2_bot')
 lig_combo2('나', BOT_DOT, 'na3_bot', MACRON, 'na3_mac')
 lig_single('낙', MACRON, 'na4_mac')
 lig_combo2('난', BOT_DOT, 'na5_bot', BOT_TILDE, 'na5_tilde')
@@ -139,7 +152,7 @@ lig_single('냣', BOT_TILDE, 'na14_tilde')
 lig_single('냥', BOT_TILDE, 'na15_tilde')
 lig_combo2('누', BOT_DOT, 'na16_bot', MACRON, 'na16_mac')
 lig_single('니', BOT_DOT, 'na17_bot')
-lig_single('네', BOT_DOT, 'na18_bot')         # 18번 아랫점으로 변경
+lig_single('네', BOT_DOT, 'na18_bot')
 
 # --- 다계열 ---
 lig_single('다', MACRON, 'da1_mac')
@@ -164,6 +177,109 @@ lig_single('띠', MACRON, 'da19_mac')
 lig_single('띠', BOT_DOT, 'da20_bot')
 lig_single('띳', BOT_DOT, 'da21_bot')
 lig_single('띤', BOT_DOT, 'da22_bot')
+
+# --- 라계열 ---
+lig_single('라', MACRON, 'ra1_mac')
+lig_combo2('라', MACRON, 'ra2_mac', BOT_DOT, 'ra2_bot')
+lig_single('랏', DOUBLE, 'ra3_double')
+lig_single('로', TOP_TILDE, 'ra4_tilde_top')
+lig_single('로', DOUBLE, 'ra5_double')
+lig_single('루', MACRON, 'ra6_mac')
+lig_combo2('리', MACRON, 'ra7_mac', BOT_DOT, 'ra7_bot')
+
+# --- 마계열 ---
+lig_single('마', MACRON, 'ma1_mac')
+lig_single('만', BOT_TILDE, 'ma2_tilde')
+lig_single('망', BOT_TRI, 'ma3_tri')
+lig_single('뭇', BOT_DOT, 'ma4_bot')
+lig_single('무', MACRON, 'ma5_mac')
+lig_single('물', MACRON, 'ma6_mac')
+
+# --- 바계열 ---
+lig_single('바', MACRON, 'ba1_mac')
+lig_single('바', TOP, 'ba2_top')
+lig_combo2('바', TOP, 'ba3_top', MACRON, 'ba3_mac')
+lig_single('뱌', MACRON, 'ba4_mac')
+lig_single('발', MACRON, 'ba5_mac')
+lig_combo2('발', MACRON, 'ba6_mac', BOT_DOT, 'ba6_bot')
+lig_single('보', TOP, 'ba7_top')
+lig_single('붕', TOP, 'ba8_top')
+lig_single('부', TOP, 'ba9_top')
+lig_combo2('부', TOP, 'ba10_top', MACRON, 'ba10_mac')
+lig_single('비', TOP, 'ba11_top')
+lig_single('빅', TOP, 'ba12_top')
+lig_combo2('빈', TOP, 'ba13_top', BOT_TILDE, 'ba13_tilde')
+lig_single('빈', TOP, 'ba14_top')
+lig_single('빗', TOP, 'ba15_top')
+lig_single('빠', MACRON, 'ba16_mac')
+lig_single('빤', BOT_DOT, 'ba17_bot')
+lig_single('빤', BOT_TILDE, 'ba18_tilde')
+lig_single('빳', BOT_DOT, 'ba19_bot')
+lig_single('뿌', MACRON, 'ba20_mac')
+lig_single('뿐', BOT_TILDE, 'ba21_tilde')
+lig_single('붓', TOP, 'ba22_top')
+lig_single('베', TOP, 'ba23_top')
+
+# --- 사계열 ---
+lig_single('사', MACRON, 'sa1_mac')
+lig_single('산', BOT_TILDE, 'sa2_tilde')
+lig_single('상', BOT_TRI, 'sa3_tri')
+lig_single('실', MACRON, 'sa4_mac')
+
+# --- 아계열 ---
+lig_single('아', MACRON, 'a1_mac')
+lig_single('안', BOT_TILDE, 'a2_tilde')
+lig_single('앗', BOT_DOT, 'a3_bot')
+lig_single('야', DOUBLE, 'a4_double')
+lig_single('야', MACRON, 'a5_mac')
+lig_combo2('야', MACRON, 'a6_mac', DOUBLE, 'a6_double')
+lig_single('얀', BOT_TILDE, 'a7_tilde')
+lig_single('오', CARON, 'a8_caron')
+lig_single('와', TOP, 'a9_top')
+lig_combo2('와', TOP, 'a10_top', MACRON, 'a10_mac')
+lig_single('완', BOT_DOT, 'a11_bot')
+lig_single('왕', TOP, 'a12_top')
+lig_single('왕', BOT_TRI, 'a13_tri')
+lig_single('웃', BOT_DOT, 'a14_bot')
+lig_combo2('웃', CARON, 'a15_caron', BOT_DOT, 'a15_bot')
+lig_single('윈', BOT_TILDE, 'a16_tilde')
+lig_single('이', DOUBLE, 'a17_double')
+lig_single('잉', DOUBLE, 'a18_double')
+lig_combo2('잇', DOUBLE, 'a19_double', BOT_DOT, 'a19_bot')
+lig_single('윈', TOP, 'a20_top')
+
+# --- 자계열 ---
+lig_single('자', MACRON, 'ja1_mac')
+lig_combo2('자', TOP, 'ja2_top', MACRON, 'ja2_mac')
+lig_single('짜', MACRON, 'ja3_mac')
+lig_single('짠', BOT_TILDE, 'ja4_tilde')
+lig_single('찐', BOT_TILDE, 'ja5_tilde')
+lig_single('찐', BOT_DOT, 'ja6_bot')
+lig_single('지', MACRON, 'ja7_mac')
+
+# --- 차계열 ---
+lig_single('차', MACRON, 'cha1_mac')
+
+# --- 카계열 ---
+lig_single('카', MACRON, 'ka1_mac')
+
+# --- 타계열 ---
+lig_single('타', MACRON, 'ta1_mac')
+lig_combo2('타', MACRON, 'ta2_mac', BOT_DOT, 'ta2_bot')
+lig_single('탑', BOT_DOT, 'ta3_bot')
+lig_combo2('티', MACRON, 'ta4_mac', BOT_DOT, 'ta4_bot')
+lig_single('티', BOT_DOT, 'ta5_bot')
+lig_single('팅', BOT_DOT, 'ta6_bot')
+lig_single('타', BOT_DOT, 'ta7_bot')
+lig_single('테', BOT_DOT, 'ta8_bot')
+
+# --- 파계열 ---
+lig_single('폿', BOT_DOT, 'pa1_bot')
+
+# --- 하계열 ---
+lig_single('후', MACRON, 'ha1_mac')
+lig_single('히', MACRON, 'ha2_mac')
+lig_single('하', MACRON, 'ha3_mac')
 
 tag = os.environ.get('OC_BUILD_TAG', 'base')
 font.fontname=f'PaliHangulV289Ga10Editor_{tag}'
